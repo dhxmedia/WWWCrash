0e96f4accc612ef59e1f3203e7074181ae7dc9d8
kwinnik <kwinnik@.(none)>
2014-02-11 11:35:38 -0500

    Run out of memory if you execute this

---
+++ Assets/WWWCrash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WWWCrash : MonoBehaviour {
+
+	IEnumerator Start()
+	{
+		while(true)
+		{
+			Debug.Log("Start DL");
+			WWW www = new WWW("http://www.dhxmedia.com/images/banners/shows/banner_03.jpg");
+			{
+				yield return www;
+			}
+			www.Dispose();
+			yield return new WaitForSeconds(1.0f);
+			Debug.Log("Finish DL");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
